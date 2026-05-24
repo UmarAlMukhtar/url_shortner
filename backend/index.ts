f9b989a020e0d66db7addd7d005677641e4f78b7
Umar Al Mukhtar <umar1868807@gmail.com>
2026-05-24 20:42:31 +0530

    feat: Convert to typescript

--- backend/index.ts
+++ backend/index.ts
@@ -1,22 +1,35 @@
-const mongoose = require("mongoose");
-const express = require("express");
-require("dotenv").config();
+import { Schema, model, connect } from 'mongoose';
+import express from 'express';
+import type { Request, Response } from 'express';
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
-const URLSchema = new mongoose.Schema(
-  { originalUrl: String, code: String },
+interface IUrl {
+  originalUrl: string
+  code: string
+  createdAt: Date
+  updatedAt: Date
+}
+
+const UrlSchema = new Schema<IUrl>({
+  originalUrl: { type: String, required: true },
+  code: { type: String, required: true },
+  },
   { timestamps: true },
 );
-const Url = mongoose.model("Url", URLSchema);
+
+const Url = model<IUrl>("Url", UrlSchema);
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.status(200).json({ status: "Success", msg: "Hello World!" });
 });
 
-app.post("/shorten", async (req, res) => {
+app.post("/shorten", async (req: Request, res: Response) => {
   const originalUrl = req.body.url;
   const urlRegex =
     /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/;
@@ -36,21 +49,24 @@ app.post("/shorten", async (req, res) => {
   }
 });
 
-app.get("/:code", async (req, res) => {
+app.get("/:code", async (req: Request<{ code: string }>, res: Response) => {
   const { code } = req.params;
   const existingUrl = await Url.findOne({ code });
   if (!existingUrl) {
     res.status(400).json({ error: "Code not found" });
     return;
   }
+
   const originalUrl = existingUrl.originalUrl;
+  if(!originalUrl) return res.status(400).json({ error: "Original URL Not found" });
   res.redirect(302, originalUrl);
 });
 
 const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI;
+if(!MONGO_URI) throw new Error("MONGO_URI not defined");
 
-mongoose
-  .connect(process.env.MONGO_URI)
+connect(MONGO_URI)
   .then(() => {
     console.log("MongoDB Connected");
     app.listen(PORT, () => {
